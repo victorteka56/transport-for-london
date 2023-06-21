c76f0ebe4adfbad52bc7660d72b9310c54ffb06f
Gemal Hamid Hisuin <gemalhamid.hisuin@miu.edu>
2023-06-21 02:21:16 +0000

    Updated RtdisServiceApplication.java

--- RTDISService/src/main/java/com/example/rtdisservice/RtdisServiceApplication.java
+++ RTDISService/src/main/java/com/example/rtdisservice/RtdisServiceApplication.java
@@ -1,13 +1,11 @@
-package com.example.rtdisservice;
+package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RtdisServiceApplication {
-
+public class RtdiServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(RtdisServiceApplication.class, args);
+        SpringApplication.run(RtdiServiceApplication.class, args);
     }
-
 }
